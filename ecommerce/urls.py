0e144216154b0c77360ad99debbe93a2df7f2109
Anurag Singh <x23180013@student.ncirl.ie>
2024-04-21 16:19:52 +0100

    added pylint fixes

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -2,13 +2,12 @@
 URL Configuration for the Mama Earth Application.
 
 This module defines the URL patterns for the various views in the E-commerce application.
-
 """
 
 from django.contrib import admin
 from django.urls import path
-from ecom import views
 from django.contrib.auth.views import LoginView, LogoutView
+from ecom import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -38,14 +37,16 @@ urlpatterns = [
     path('update-order/<int:pk>', views.update_order_view, name='update-order'),
 
     path('customersignup', views.customer_signup_view),
-    path('customerlogin', LoginView.as_view(template_name='ecom/customerlogin.html'), name='customerlogin'),
+    path('customerlogin', LoginView.as_view(template_name='ecom/customerlogin.html')
+        , name='customerlogin'),
     path('customer-home', views.customer_home_view, name='customer-home'),
     path('my-order', views.my_order_view, name='my-order'),
 
     # path('my-order', views.my_order_view2,name='my-order'),
     path('my-profile', views.my_profile_view, name='my-profile'),
     path('edit-profile', views.edit_profile_view, name='edit-profile'),
-    path('download-invoice/<int:orderID>/<int:productID>', views.download_invoice_view, name='download-invoice'),
+    path('download-invoice/<int:orderID>/<int:productID>',
+        views.download_invoice_view, name='download-invoice'),
 
     path('add-to-cart/<int:pk>', views.add_to_cart_view, name='add-to-cart'),
     path('cart', views.cart_view, name='cart'),
